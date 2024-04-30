268f90c2ba0a0a45e70faf853d9b2f938c142433
Ryan Keemink <Keemink.ryan@gmail.com>
2024-04-30 22:42:22 +0200

    added a Fen parser

--- engine.py
+++ engine.py
@@ -10,4 +10,22 @@ class chessEngine:
     
 
     def FENParser(self, FENString):
+        FEN = FENString.split(" ")
+        board = FEN[0].split("/")
+        for i in range(8):
+            j = 0
+            for c in board[i]:
+                if c.isnumeric():
+                    j += int(c)
+                else:
+                    self.board[i][j] = c
+                    j += 1
+        self.turn = FEN[1]
+        self.castle = FEN[2]
+        self.enpassant = FEN[3]
+        self.halfmove = FEN[4]
+        self.fullmove = FEN[5]
+
+    
+
         
